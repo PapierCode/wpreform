165e65f16af38f9c140eb8eb4572be62967a2d20
julien <julien@papier-code.fr>
2020-03-11 17:24:18 +0100

    CSS & dev

--- include/head.php
+++ include/head.php
@@ -51,10 +51,9 @@ add_action( 'wp_enqueue_scripts', 'pc_enqueue_preform_style' );
 		
 		// par defaut
 		$font = get_template_directory_uri().'/css/font-face/font-face.css';
-		// pour modifier
-		// ou retourner une chaine vide pour désactiver
+		// pour modifier ou retourner une chaine vide pour désactiver
 		$font = apply_filters( 'pc_filter_font_face', $font );
-		// affichage si non vide
+		// chargement si non vide
 		if (  '' != trim($font) ) {
 			wp_enqueue_style( 'preform-font-face', $font, null, null, 'screen');
 		}
