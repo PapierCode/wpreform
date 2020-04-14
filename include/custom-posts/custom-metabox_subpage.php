19a1887e6383add976e8dd278f63883c3b210bc8
julien <julien@papier-code.fr>
2020-04-14 15:28:22 +0200

    V1.0.1 ajout filtre pc_filter_metabox_select_content_from

--- include/custom-posts/custom-metabox_subpage.php
+++ include/custom-posts/custom-metabox_subpage.php
@@ -13,7 +13,7 @@
 add_action( 'admin_init', function() {
 
     global $settings_project;
-    $box_content_more_title = ( count( $settings_project['page-content-from'] ) > 0 ) ? 'Contenu supplémentaire' : 'Sous-pages';
+    $box_content_more_title = ( !empty( $settings_project['page-content-from'] ) ) ? 'Contenu supplémentaire' : 'Sous-pages';
 
     add_meta_box(
         'page-content-sup',
@@ -82,7 +82,7 @@ function pc_page_content_sub( $post ) {
 
     // début mise en page
     if ( $post->post_parent < 1 ) { // si parent ou à devenir
-        if ( count( $settings_project['page-content-from'] ) > 0 ) {
+        if ( !empty( $settings_project['page-content-from'] ) ) {
             echo '<p><strong>Sélectionnez un contenu spécifique <strong style="font-weight:700">OU</strong> des sous-pages.</strong></p>';
         }
         echo '<p><em><strong>Remarque :</strong> lorsqu\'une page devient sous-page, son adresse (URL) est préfixée avec l\'adresse de la page parent, pour indiquer la hiérarchie.</em></p>';
@@ -96,15 +96,18 @@ function pc_page_content_sub( $post ) {
     =            Sélection d'un contenu formaté            =
     ======================================================*/
 
-    if ( count( $settings_project['page-content-from'] ) > 0 ) {
-    
+    if ( !empty( $settings_project['page-content-from'] ) ) {
+	
+		// filtre
+		$metabox_select_content_from = $settings_project['page-content-from'];
+		$metabox_select_content_from = apply_filters( 'pc_filter_metabox_select_content_from', $metabox_select_content_from, $post );
         // en bdd
         $content_from_saved = get_post_meta( $post->ID, 'content-from', true );
 
         // affichage
         echo '<tr><th><label for="content-from">Contenu spécifique</label></th><td>';
             echo '<select id="content-from" name="content-from"><option value=""></option>';
-            foreach ( $settings_project['page-content-from'] as $slug => $datas ) {
+            foreach ( $metabox_select_content_from as $slug => $datas ) {
                 echo '<option value="'.$slug.'" '.selected($content_from_saved,$slug,false).'>'.$datas[0].'</option>';
             }
             echo '</select>';
@@ -177,78 +180,78 @@ function pc_page_content_sub( $post ) {
 
 add_action( 'save_post', 'pc_sub_page_save' );
 
-function pc_sub_page_save( $post_id ) {
+	function pc_sub_page_save( $post_id ) {
 
-    // check input hidden de vérification
-    if ( isset($_POST['none-page-content-sup']) && wp_verify_nonce( $_POST['none-page-content-sup'], basename( __FILE__ ) ) ) {
+		// check input hidden de vérification
+		if ( isset($_POST['none-page-content-sup']) && wp_verify_nonce( $_POST['none-page-content-sup'], basename( __FILE__ ) ) ) {
 
-        // champs à traiter
-        $fields = array(
-            'content-from' => $_POST['content-from'],
-            'content-subpages' => $_POST['content-subpages'],
-        );
+			// champs à traiter
+			$fields = array(
+				'content-from' => $_POST['content-from'],
+				'content-subpages' => $_POST['content-subpages'],
+			);
 
-        foreach ($fields as $name => $value) {
+			foreach ($fields as $name => $value) {
 
-            // valeur renvoyée par le form
-            $temp = $value;
-            // valeur en bdd
-            $save = get_post_meta( $post_id, $name, true );
+				// valeur renvoyée par le form
+				$temp = $value;
+				// valeur en bdd
+				$save = get_post_meta( $post_id, $name, true );
 
 
-            /*----------  Traitement des sous-pages  ----------*/
-            
-            // si c'est la liste de sous-pages
-            if ( $name == 'content-subpages' ) {
-
-                $subpages_temp = explode(',',$temp);
-				$subpages_saved = explode(',',$save);
+				/*----------  Traitement des sous-pages  ----------*/
 				
-				//$real_post_id = ( wp_is_post_revision( $post_id ) ) ? wp_is_post_revision( $post_id ) : $post_id;
-                
-                // nouvelle liste de sous-pages
-                if ( $temp != '' && count($subpages_temp) > 0 ) {
-                    foreach ($subpages_temp as $temp_id) {
-                        // si ce n'est pas déjà une sous-page
-                        if ( !in_array($temp_id,$subpages_saved) ) {
-                            pc_update_subpage( $temp_id, $post_id );
-                        // si c'est déjà une sous-page
-                        } else {
-                            // suppression dans le tableau représentant la sauvegarde
-                            // voir "sous-page à détacher" ci-dessous
-                            unset($subpages_saved[array_search($temp_id, $subpages_saved)]);
-                        }
-                    }
-                }
+				// si c'est la liste de sous-pages
+				if ( $name == 'content-subpages' ) {
 
-                // sous-page à détacher 
-                if ( $save != '' && count($subpages_saved) > 0 ) {
-                    foreach ($subpages_saved as $save_id) { pc_update_subpage( $save_id, '' ); }
+					$subpages_temp = explode(',',$temp);
+					$subpages_saved = explode(',',$save);
+					
+					//$real_post_id = ( wp_is_post_revision( $post_id ) ) ? wp_is_post_revision( $post_id ) : $post_id;
+					
+					// nouvelle liste de sous-pages
+					if ( $temp != '' && count($subpages_temp) > 0 ) {
+						foreach ($subpages_temp as $temp_id) {
+							// si ce n'est pas déjà une sous-page
+							if ( !in_array($temp_id,$subpages_saved) ) {
+								pc_update_subpage( $temp_id, $post_id );
+							// si c'est déjà une sous-page
+							} else {
+								// suppression dans le tableau représentant la sauvegarde
+								// voir "sous-page à détacher" ci-dessous
+								unset($subpages_saved[array_search($temp_id, $subpages_saved)]);
+							}
+						}
+					}
+
+					// sous-page à détacher 
+					if ( $save != '' && count($subpages_saved) > 0 ) {
+						foreach ($subpages_saved as $save_id) { pc_update_subpage( $save_id, '' ); }
+					}
+					
 				}
+
+
+				/*----------  Traitement du champ  ----------*/
 				
-            }
+				// si une valeur arrive & si rien en bdd
+				if ( $temp && '' == $save ) {
+					add_post_meta( $post_id, $name, $temp, true );
 
+				// si une valeur arrive & différente de la bdd
+				} elseif ( $temp && $temp != $save ) {
+					update_post_meta( $post_id, $name, $temp );
 
-            /*----------  Traitement du champ  ----------*/
-            
-            // si une valeur arrive & si rien en bdd
-            if ( $temp && '' == $save ) {
-                add_post_meta( $post_id, $name, $temp, true );
+				// si rien n'arrive & si un truc en bdd
+				} elseif ( '' == $temp && $save ) {
+					delete_post_meta( $post_id, $name );
+				}
 
-            // si une valeur arrive & différente de la bdd
-            } elseif ( $temp && $temp != $save ) {
-                update_post_meta( $post_id, $name, $temp );
+			};
 
-            // si rien n'arrive & si un truc en bdd
-            } elseif ( '' == $temp && $save ) {
-                delete_post_meta( $post_id, $name );
-            }
+		}
 
-        };
-
-    }
-
-}
-    
+	}
+		
 
 /*=====  FIN Sauvegarde  =====*/
