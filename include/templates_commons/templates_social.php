614643d521b1a8580abe669e1d465d53d44143ce
julien <julien@papier-code.fr>
2020-12-15 11:35:12 +0100

    1.2.8 ajout rel noreferrer sur les liens sortants (templates)

--- include/templates_commons/templates_social.php
+++ include/templates_commons/templates_social.php
@@ -27,7 +27,7 @@ function pc_display_social_links( $css_class ) {
 
 			if ( !$ul ) { echo '<ul class="social-list reset-list no-print '.$css_class.'">'; $ul = true; };
 
-			echo '<li class="social-item"><a class="social-link social-link--'.$field['label_for'].'" href="'.$settings_project[$id].'" title="'.$field['label'].' (nouvelle fenêtre)" target="_blank"><span class="visually-hidden">'.$field['label'].'</span>'.pc_svg($field['label_for']).'</a></li>';		
+			echo '<li class="social-item"><a class="social-link social-link--'.$field['label_for'].'" href="'.$settings_project[$id].'" title="'.$field['label'].' (nouvelle fenêtre)" target="_blank" rel="noreferrer"><span class="visually-hidden">'.$field['label'].'</span>'.pc_svg($field['label_for']).'</a></li>';		
 			
 		}
 
@@ -55,19 +55,19 @@ function pc_display_share_links() {
         <p class="social-share-title">Partage&nbsp;: </p>
         <ul class="social-list social-list--share reset-list">
             <li class="social-item">
-                <a href="https://www.facebook.com/sharer/sharer.php?u=<?= urlencode($share_url); ?>" target="_blank" class="social-link social-link--facebook" rel="nofollow" title="Partager sur Facebook (nouvelle fenêtre)">
+                <a href="https://www.facebook.com/sharer/sharer.php?u=<?= urlencode($share_url); ?>" target="_blank" class="social-link social-link--facebook" rel="nofollow noreferrer" title="Partager sur Facebook (nouvelle fenêtre)">
                     <span class="visually-hidden">Facebook</span>
                     <?php echo pc_svg('facebook'); ?>
                 </a>
             </li>
             <li class="social-item">
-                <a href="http://twitter.com/intent/tweet?url=<?= urlencode($share_url); ?>" target="_blank" class="social-link social-link--twitter" rel="nofollow" title="Partager sur Twitter (nouvelle fenêtre)">
+                <a href="http://twitter.com/intent/tweet?url=<?= urlencode($share_url); ?>" target="_blank" class="social-link social-link--twitter" rel="nofollow noreferrer" title="Partager sur Twitter (nouvelle fenêtre)">
                     <span class="visually-hidden">Twitter</span>
                     <?php echo pc_svg('twitter'); ?>
                 </a>
             </li>
             <li class="social-item">
-                <a href="https://www.linkedin.com/shareArticle?mini=true&url=<?= urlencode($share_url); ?>&title=<?= str_replace(' ', '%20', $meta_title); ?>&summary=<?= str_replace(' ', '%20', $meta_description); ?>" target="_blank" class="social-link social-link--linkedin" rel="nofollow" title="Partager sur LinkedIn (nouvelle fenêtre)">
+                <a href="https://www.linkedin.com/shareArticle?mini=true&url=<?= urlencode($share_url); ?>&title=<?= str_replace(' ', '%20', $meta_title); ?>&summary=<?= str_replace(' ', '%20', $meta_description); ?>" target="_blank" class="social-link social-link--linkedin" rel="nofollow noreferrer" title="Partager sur LinkedIn (nouvelle fenêtre)">
                 <span class="visually-hidden">LinkedIn</span>
                 <?php echo pc_svg('linkedin'); ?>
                 </a>
