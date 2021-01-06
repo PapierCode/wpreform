c08ef400a5dcdc99e95482a451241b3ddc6180c7
julien <julien@papier-code.fr>
2021-01-06 16:22:52 +0100

    1.3.0 option page protégée par mot de passe

--- include/template_page.php
+++ include/template_page.php
@@ -42,7 +42,9 @@ function pc_display_schema_post( $post, $post_metas ) {
 	$schema = pc_get_schema_article( $post, $post_metas, true );
 	$schema = apply_filters( 'pc_filter_schema_post', $schema, $post, $post_metas );
 
-	echo '<script type="application/ld+json">'.json_encode($schema,JSON_UNESCAPED_SLASHES).'</script>';
+	if ( !post_password_required() ) {
+		echo '<script type="application/ld+json">'.json_encode($schema,JSON_UNESCAPED_SLASHES).'</script>';
+	}
 
 }
 
@@ -55,7 +57,7 @@ function pc_display_schema_post( $post, $post_metas ) {
 
 function pc_display_st_list_start( $post, $post_metas ) {
 
-	if ( isset( $post_metas['content-subpages'] ) ) {
+	if ( !post_password_required() && isset( $post_metas['content-subpages'] ) ) {
 		echo '<div class="st-list">';
 	}
 
@@ -63,52 +65,56 @@ function pc_display_st_list_start( $post, $post_metas ) {
 
 function pc_display_specific_content( $post, $post_metas ) {
 
-	/*----------  Contenu spécifique  ----------*/
-		
-	if ( isset($post_metas['content-from']) ) {
+	if ( !post_password_required() ) {
 
-		global $settings_project;
-		include $settings_project['page-content-from'][$post_metas['content-from'][0]][1];
+		/*----------  Contenu spécifique  ----------*/
+			
+		if ( isset($post_metas['content-from']) ) {
+
+			global $settings_project;
+			include $settings_project['page-content-from'][$post_metas['content-from'][0]][1];
 
 
-	/*----------  Sous-pages  ----------*/		
+		/*----------  Sous-pages  ----------*/		
 
-	} else if ( isset($post_metas['content-subpages']) ) {
+		} else if ( isset($post_metas['content-subpages']) ) {
 
-		// liste
-		$sub_pages_ids = explode( ',', $post_metas['content-subpages'][0] );
-		// données structurées
-		$sub_pages_schema = array(
-			'@context' => 'http://schema.org/',
-			'@type'=> 'CollectionPage',
-			'mainEntity' => array(
-				'@type' => 'ItemList',
-				'itemListElement' => array()
-			),
-			'isPartOf' => pc_get_schema_article( $post, $post_metas, $sub_pages_ids )
-		);
-
-		// affichage des résumés de pages
-		foreach ( $sub_pages_ids as $key => $post_id ) {
-			pc_display_post_resum( $post_id, '', 2 );
+			// liste
+			$sub_pages_ids = explode( ',', $post_metas['content-subpages'][0] );
 			// données structurées
-			global $post_resum_schema;
-			$st_schema['position'] = $key + 1;
-			$sub_pages_schema['mainEntity']['itemListElement'][] = $post_resum_schema;
+			$sub_pages_schema = array(
+				'@context' => 'http://schema.org/',
+				'@type'=> 'CollectionPage',
+				'mainEntity' => array(
+					'@type' => 'ItemList',
+					'itemListElement' => array()
+				),
+				'isPartOf' => pc_get_schema_article( $post, $post_metas, $sub_pages_ids )
+			);
+
+			// affichage des résumés de pages
+			foreach ( $sub_pages_ids as $key => $post_id ) {
+				pc_display_post_resum( $post_id, '', 2 );
+				// données structurées
+				global $post_resum_schema;
+				$st_schema['position'] = $key + 1;
+				$sub_pages_schema['mainEntity']['itemListElement'][] = $post_resum_schema;
+			}
+
+			do_action( 'pc_st_list_fake', count($sub_pages_ids), '' );
+
+			// affichage des données structurées
+			echo '<script type="application/ld+json">'.json_encode( $sub_pages_schema, JSON_UNESCAPED_SLASHES ).'</script>';
+
 		}
 
-		do_action( 'pc_st_list_fake', count($sub_pages_ids), '' );
-
-		// affichage des données structurées
-		echo '<script type="application/ld+json">'.json_encode( $sub_pages_schema, JSON_UNESCAPED_SLASHES ).'</script>';
-
-	}
+	} // FIN if !post_password_required()
 
 }
 
 function pc_display_st_list_end( $post, $post_metas ) {
 
-	if ( isset( $post_metas['content-subpages'] ) ) {
+	if ( !post_password_required() && isset( $post_metas['content-subpages'] ) ) {
 		echo '</div>';
 	}
 
