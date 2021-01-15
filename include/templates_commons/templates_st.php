a59faf6ae54a30dc151cf6a75d5e37b91ae4a191
julien <julien@papier-code.fr>
2021-01-15 09:55:23 +0100

    1.4.4 renommage variables résumé post

--- include/templates_commons/templates_st.php
+++ include/templates_commons/templates_st.php
@@ -28,18 +28,18 @@ function pc_get_page_excerpt( $post_id, $post_metas, $seo_for = false ) {
 
 	if ( $seo_for && isset( $post_metas['seo-desc'] ) && $post_metas['seo-desc'][0] != '' ) {
 
-		$excerpt = $post_metas['seo-desc'][0];
+		$post_excerpt = $post_metas['seo-desc'][0];
 
 	} else if ( isset( $post_metas['resum-desc'] ) && $post_metas['resum-desc'][0] != '' ) {
 
-		$excerpt = $post_metas['resum-desc'][0];
+		$post_excerpt = $post_metas['resum-desc'][0];
 
 	} else {
 
-		$excerpt = get_the_excerpt( $post_id );
+		$post_excerpt = get_the_excerpt( $post_id );
 	}
 	
-	return $excerpt;
+	return $post_excerpt;
 
 }
 
@@ -54,16 +54,16 @@ function pc_get_page_excerpt( $post_id, $post_metas, $seo_for = false ) {
 
 function pc_get_post_resum_img_default_datas() {
 
-	$dir = get_bloginfo('template_directory');
+	$template_directory = get_bloginfo('template_directory');
 
-	$img_datas = array(
-		$dir.'/images/st-default-400.jpg',
-		$dir.'/images/st-default-500.jpg',
-		$dir.'/images/st-default-700.jpg'
+	$post_img_datas = array(
+		$template_directory.'/images/st-default-400.jpg',
+		$template_directory.'/images/st-default-500.jpg',
+		$template_directory.'/images/st-default-700.jpg'
 	);
 
-	$img_datas = apply_filters( 'pc_filter_post_resum_img_default_datas', $img_datas );
-	return $img_datas;
+	$post_img_datas = apply_filters( 'pc_filter_post_resum_img_default_datas', $post_img_datas );
+	return $post_img_datas;
 
 }
 
@@ -73,41 +73,41 @@ function pc_get_post_resum_img_default_datas() {
 function pc_get_post_resum_img_datas( $post_id, $post_metas ) {
 
 	$img_post = ( isset($post_metas['visual-id']) ) ? get_post( $post_metas['visual-id'][0] ) : null;
-	$img_datas = array();
+	$post_img_datas = array();
 
 	if ( is_object( $img_post ) ) {
 
-		$img_datas['urls'] = array(
+		$post_img_datas['urls'] = array(
 			wp_get_attachment_image_src( $img_post->ID,'st-400' )[0],
 			wp_get_attachment_image_src( $img_post->ID,'st-500' )[0],
 			wp_get_attachment_image_src( $img_post->ID,'st-700' )[0]
 		);
-		$img_datas['alt'] = get_post_meta( $img_post->ID, '_wp_attachment_image_alt', true );	
+		$post_img_datas['alt'] = get_post_meta( $img_post->ID, '_wp_attachment_image_alt', true );	
 
 	} else {
 
-		$img_datas['urls'] = pc_get_post_resum_img_default_datas();
-		$img_datas['alt'] = '';
+		$post_img_datas['urls'] = pc_get_post_resum_img_default_datas();
+		$post_img_datas['alt'] = '';
 
 	}
 
-	apply_filters( 'pc_filter_post_resum_img_datas', $img_datas, $post_id, $post_metas );
-	return $img_datas;
+	apply_filters( 'pc_filter_post_resum_img_datas', $post_img_datas, $post_id, $post_metas );
+	return $post_img_datas;
 
 }
 
 
 /*----------  HTML  ----------*/
 
-function pc_display_post_resum_img_tag( $post_id, $img_datas ) {
+function pc_display_post_resum_img_tag( $post_id, $post_img_datas ) {
 
-	$img_tag_srcset = $img_datas['urls'][0].' 400w, '.$img_datas['urls'][1].' 500w, '.$img_datas['urls'][2].' 700w';
-	$img_tag_sizes = '(max-width:400px) 400px, (min-width:401px) and (max-width:759px) 700px, (min-width:760px) 500px';
+	$post_img_tag_srcset = $post_img_datas['urls'][0].' 400w, '.$post_img_datas['urls'][1].' 500w, '.$post_img_datas['urls'][2].' 700w';
+	$post_img_tag_sizes = '(max-width:400px) 400px, (min-width:401px) and (max-width:759px) 700px, (min-width:760px) 500px';
 
-	$img_tag = '<img src="'.$img_datas['urls'][2].'" alt="'.$img_datas['alt'].'" srcset="'.$img_tag_srcset.'" sizes="'.$img_tag_sizes.'" loading="lazy" />';
+	$post_img_tag = '<img src="'.$post_img_datas['urls'][2].'" alt="'.$post_img_datas['alt'].'" srcset="'.$post_img_tag_srcset.'" sizes="'.$post_img_tag_sizes.'" loading="lazy" />';
 
-	$img_tag = apply_filters( 'pc_filter_post_resum_img_tag', $img_tag, $post_id );
-	echo $img_tag;
+	$post_img_tag = apply_filters( 'pc_filter_post_resum_img_tag', $post_img_tag, $post_id );
+	echo $post_img_tag;
 
 }
 
@@ -118,21 +118,21 @@ function pc_display_post_resum_img_tag( $post_id, $img_datas ) {
 =            Affichage            =
 =================================*/
 
-function pc_display_post_resum( $post_id, $css = '', $hn = 2 ) {
+function pc_display_post_resum( $post_id, $post_css = '', $post_title_level = 2 ) {
 
 	// post métas
-	$metas = get_post_meta($post_id);
+	$post_metas = get_post_meta($post_id);
 
 	// titre
-	$title = (isset($metas['resum-title'])) ? $metas['resum-title'][0] : get_the_title($post_id);
+	$post_title = (isset($post_metas['resum-title'])) ? $post_metas['resum-title'][0] : get_the_title($post_id);
 	// lien
-	$url = get_the_permalink($post_id);
+	$post_url = get_the_permalink($post_id);
 	// image
-	$img_datas = pc_get_post_resum_img_datas( $post_id, $metas );
+	$post_img_datas = pc_get_post_resum_img_datas( $post_id, $post_metas );
 	// icône +	
 	$ico_more = apply_filters( 'pc_filter_post_resum_ico_more', pc_svg('more-16') );
 	// description
-	$desc = pc_get_page_excerpt( $post_id, $metas );
+	$post_desc = pc_get_page_excerpt( $post_id, $post_metas );
 	
 
 	/*----------  Données structurées  ----------*/
@@ -141,12 +141,12 @@ function pc_display_post_resum( $post_id, $css = '', $hn = 2 ) {
 
 	$post_resum_schema = array(
 		'@type' => 'ListItem',
-		'name' => $title,
-		'description' => $desc,
-		'url' => $url,
+		'name' => $post_title,
+		'description' => $post_desc,
+		'url' => $post_url,
 		'image' => array(
 			'@type'		=>'ImageObject',
-			'url' 		=> $img_datas['urls'][2],
+			'url' 		=> $post_img_datas['urls'][2],
 			'width' 	=> $images_project_sizes['st-700']['width'],
 			'height' 	=> $images_project_sizes['st-700']['height']
 		)
@@ -155,21 +155,21 @@ function pc_display_post_resum( $post_id, $css = '', $hn = 2 ) {
 
 	/*----------  Affichage  ----------*/
 	
-	echo '<article class="st '.$css.'"><div class="st-inner">';
+	echo '<article class="st '.$post_css.'"><div class="st-inner">';
 
 		// filtre
 		do_action( 'pc_action_post_resum_after_start', $post_id );
 	
 		echo '<figure class="st-figure">';
-			pc_display_post_resum_img_tag( $post_id, $img_datas );
+			pc_display_post_resum_img_tag( $post_id, $post_img_datas );
 		echo '</figure>';
 
-		echo '<h'.$hn.' class="st-title"><a href="'.$url.'">'.$title.'</a></h'.$hn.'>';	
+		echo '<h'.$post_title_level.' class="st-title"><a href="'.$post_url.'">'.$post_title.'</a></h'.$post_title_level.'>';	
 
 		// filtre	
 		do_action( 'pc_action_post_resum_after_title', $post_id );
 		
-		echo '<p class="st-desc">'.$desc.'... <span>'.$ico_more.'</span></p>';
+		echo '<p class="st-desc">'.$post_desc.'... <span>'.$ico_more.'</span></p>';
 	
 		// filtre
 		do_action( 'pc_action_post_resum_before_end', $post_id );
