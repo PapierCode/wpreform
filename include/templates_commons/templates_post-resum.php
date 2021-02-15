a2e1eb01bba102cce75a809d776aae58f1113789
julien <julien@papier-code.fr>
2021-02-15 16:24:23 +0100

    1.9.0 amélioration accessibilité

--- include/templates_commons/templates_post-resum.php
+++ include/templates_commons/templates_post-resum.php
@@ -39,7 +39,8 @@ function pc_get_page_excerpt( $post_id, $post_metas, $seo_for = false ) {
 
 	} else {
 
-		$post_excerpt = get_the_excerpt( $post_id );
+		$post_excerpt = get_the_excerpt( $post_id ).'...';
+		
 	}
 	
 	return $post_excerpt;
@@ -121,6 +122,12 @@ function pc_display_post_resum_img_tag( $post_id, $post_img_datas ) {
 =            Affichage            =
 =================================*/
 
+function get_post_resum_link_tag_start( $class, $href, $title ) {
+
+	return '<a href="'.$href.'" class="'.$class.'" title="Lire la suite de l\'article '.$title.'">';
+
+}
+
 function pc_display_post_resum( $post_id, $post_css = '', $post_title_level = 2 ) {
 
 	// post métas
@@ -130,14 +137,13 @@ function pc_display_post_resum( $post_id, $post_css = '', $post_title_level = 2 
 	$post_title = (isset($post_metas['resum-title'])) ? $post_metas['resum-title'][0] : get_the_title($post_id);
 	// lien
 	$post_link = get_the_permalink($post_id);
-	$post_link_tag_start = '<a href="'.$post_link.'" title="Lire la suite de l\'article '.$post_title.'">';
 	$post_link_position = apply_filters( 'pc_filter_post_resum_link_postion', 'multiple' ); // multiple || global	
 	// image datas
 	$post_img_datas = pc_get_post_resum_img_datas( $post_id, $post_metas );
-	// icône +	
-	$ico_more = apply_filters( 'pc_filter_post_resum_ico_more', pc_svg('more-16') );
 	// description
 	$post_desc = pc_get_page_excerpt( $post_id, $post_metas );
+	// icône +	
+	$post_ico_more = apply_filters( 'pc_filter_post_resum_ico_more', pc_svg('more-16') );
 	
 
 	/*----------  Données structurées  ----------*/
@@ -161,24 +167,18 @@ function pc_display_post_resum( $post_id, $post_css = '', $post_title_level = 2 
 	
 	echo '<article class="st '.$post_css.'"><div class="st-inner">';
 
-		if ( 'global' == $post_link_position ) { echo $post_link_tag_start; }
+		if ( 'global' == $post_link_position ) { echo get_post_resum_link_tag_start( 'st-link', $post_link, $post_title ); }
 
 			// filtre
 			do_action( 'pc_action_post_resum_after_start', $post_id );
 		
 			echo '<figure class="st-figure">';
-				if ( 'multiple' == $post_link_position ) {
-					echo $post_link_tag_start;
-						pc_display_post_resum_img_tag( $post_id, $post_img_datas );
-					echo '</a>';
-				} else {
-					pc_display_post_resum_img_tag( $post_id, $post_img_datas );
-				}				
+				pc_display_post_resum_img_tag( $post_id, $post_img_datas );				
 			echo '</figure>';
 
 			echo '<h'.$post_title_level.' class="st-title">';
 				if ( 'multiple' == $post_link_position ) {
-					echo $post_link_tag_start.$post_title.'</a>';
+					echo get_post_resum_link_tag_start( 'st-title-link', $post_link, $post_title ).$post_title.'</a>';
 				} else {
 					echo $post_title;
 				}
@@ -188,12 +188,14 @@ function pc_display_post_resum( $post_id, $post_css = '', $post_title_level = 2 
 			do_action( 'pc_action_post_resum_after_title', $post_id );
 			
 			echo '<p class="st-desc">'.$post_desc.' ';
-				if ( 'multiple' == $post_link_position ) {
-					echo $post_link_tag_start.'<span>'.$ico_more.'</span><span class="visually-hidden">Lire la suite</span></a>';
-				} else {
-					echo '<span>'.$ico_more.'</span>';
-				}	
+				if ( 'global' == $post_link_position ) { echo '<span class="st-desc-ico">'.$post_ico_more.'</span>';	}	
 			echo '</p>';
+			
+			if ( 'multiple' == $post_link_position ) {
+				echo get_post_resum_link_tag_start( 'st-read-more', $post_link, $post_title ).'<span class="st-read-more-ico">'.$post_ico_more.'</span> <span class="st-read-more-txt">Lire la suite</span><span class="visually-hidden"> de l\'article '.$post_title.'</span></a>';
+			} else {
+				echo '<span class="st-ico">'.$post_ico_more.'</span>';
+			}
 		
 			// filtre
 			do_action( 'pc_action_post_resum_before_end', $post_id );
