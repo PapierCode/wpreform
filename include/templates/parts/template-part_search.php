c0537fc03b034e01458af86e105e43a24d115bb2
julien <julien@papier-code.fr>
2021-04-21 20:32:41 +0200

    2.0.9 recherche dans les métas, corrections hook

--- include/templates/parts/template-part_search.php
+++ include/templates/parts/template-part_search.php
@@ -1,68 +1,89 @@
 <?php
 /**
  * 
- * Communs templates : recherche
+ * Customisation de la recherche
  * 
  */
 
-
-$table_meta_name = _get_meta_table('post');
 
 /*=======================================================================
 =            Indexation des customs fields pour la recherche            =
 =======================================================================*/
 
-// Extend WordPress search to include custom fields
-// https://adambalee.com
+/**
+ * 
+ * Extend WordPress search to include custom fields
+ *
+ * https://adambalee.com
+ * 
+ */
 
-// Join posts and postmeta tables
-// http://codex.wordpress.org/Plugin_API/Filter_Reference/posts_join
+/**
+ *
+ *  Join posts and postmeta tables
+ *
+ * http://codex.wordpress.org/Plugin_API/Filter_Reference/posts_join
+ * 
+ */
 
 function pc_search_join( $join ) {
-    global $wpdb, $table_meta_name;
 
-    if ( is_admin() || is_search() ) {
-        //$join .= ' LEFT JOIN '.$wpdb->postmeta. ' ON '. $wpdb->posts . '.ID = ' . $wpdb->postmeta . '.post_id ';
-        $join .= ' LEFT JOIN '.$wpdb->postmeta. ' '.$table_meta_name.' ON '. $wpdb->posts . '.ID = '.$table_meta_name.'.post_id ';
+    global $wpdb;
+
+    if ( is_search() ) {    
+        $join .=' LEFT JOIN '.$wpdb->postmeta. ' ON '. $wpdb->posts . '.ID = ' . $wpdb->postmeta . '.post_id ';
     }
 
     return $join;
+
 }
-add_filter('posts_join', 'pc_search_join' );
 
+add_filter( 'posts_join', 'pc_search_join' );
 
-// Modify the search query with posts_where
-// http://codex.wordpress.org/Plugin_API/Filter_Reference/posts_where
+/**
+ * 
+ * Modify the search query with posts_where
+ *
+ * http://codex.wordpress.org/Plugin_API/Filter_Reference/posts_where
+ * 
+ */
 
 function pc_search_where( $where ) {
-    global $pagenow, $wpdb, $table_meta_name;
 
-    if ( is_admin() || is_search() ) {
-        // $where = preg_replace(
-        //     "/\(\s*".$wpdb->posts.".post_title\s+LIKE\s*(\'[^\']+\')\s*\)/",
-        //     "(".$wpdb->posts.".post_title LIKE $1) OR (".$wpdb->postmeta.".meta_value LIKE $1)", $where );
+    global $pagenow, $wpdb;
+
+    if ( is_search() ) {
         $where = preg_replace(
             "/\(\s*".$wpdb->posts.".post_title\s+LIKE\s*(\'[^\']+\')\s*\)/",
-            "(".$wpdb->posts.".post_title LIKE $1) OR ('.$table_meta_name.'.meta_value LIKE $1)", $where );
+            "(".$wpdb->posts.".post_title LIKE $1) OR (".$wpdb->postmeta.".meta_value LIKE $1)", $where );
     }
 
     return $where;
+
 }
+
 add_filter( 'posts_where', 'pc_search_where' );
 
-
-// Prevent duplicates
-// http://codex.wordpress.org/Plugin_API/Filter_Reference/posts_distinct
+/**
+ * 
+ * Prevent duplicates
+ *
+ * http://codex.wordpress.org/Plugin_API/Filter_Reference/posts_distinct
+ * 
+ */
 
 function pc_search_distinct( $where ) {
+
     global $wpdb;
 
-    if ( is_admin() || is_search() ) {
+    if ( is_search() ) {
         return "DISTINCT";
     }
 
     return $where;
+
 }
+
 add_filter( 'posts_distinct', 'pc_search_distinct' );
 
 
