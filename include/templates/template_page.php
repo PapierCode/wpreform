85fa6867f98ddcae75a2be44911da9b041506c65
PapierCode <julien@papier-code.fr>
2023-01-20 17:22:33 +0100

    2.17.4 déplacement données structurées article

--- include/templates/template_page.php
+++ include/templates/template_page.php
@@ -71,6 +71,13 @@ function pc_display_main_breadcrumb( $pc_post ) {
 
 function pc_display_page_wysiwyg( $pc_post ) {
 
+	// schéma Article
+	if ( apply_filters( 'pc_filter_page_schema_article_display', true, $pc_post ) ) {
+		echo '<script type="application/ld+json">';
+			echo json_encode( $pc_post->get_schema_article(), JSON_UNESCAPED_SLASHES );
+		echo '</script>';
+	}
+
 	// contenu
 	if ( apply_filters( 'pc_filter_page_wysiwyg_display', true, $pc_post ) && '' != $pc_post->content ) {
 
@@ -80,13 +87,6 @@ function pc_display_page_wysiwyg( $pc_post ) {
 			the_content();
 		if ( $display_container ) { echo '</div></div>'; }
 		
-	}
-
-	// schéma Article
-	if ( apply_filters( 'pc_filter_page_schema_article_display', true, $pc_post ) ) {
-		echo '<script type="application/ld+json">';
-			echo json_encode( $pc_post->get_schema_article(), JSON_UNESCAPED_SLASHES );
-		echo '</script>';
 	}
 
 }
